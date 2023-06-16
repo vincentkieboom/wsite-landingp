eef8e39d782f749c3befe1746b9e2d083338932e
vincentkieboom <66480272+vincentkieboom@users.noreply.github.com>
2023-06-16 08:10:32 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // NAVBAR
+    $('.navbar-nav .nav-link').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 50) {
+            $(".navbar").addClass("sticky-nav");
+        } else {
+            $(".navbar").removeClass("sticky-nav");
+        }
+    });
+
+    // BACKSTRETCH SLIDESHOW
+    $('#section_1').backstretch([
+      "images/slide/coffee.jpg", 
+      "images/slide/coffee.jpg"
+    ],  {duration: 9000, fade: 750});
+    
+  })(window.jQuery);
+
+
